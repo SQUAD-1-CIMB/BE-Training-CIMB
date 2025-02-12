6df2e72aee8fbefc16cd3e0de4dbc29356d946e0
ridhoafwani <zikrurridhoafwani@gmail.com>
2025-02-12 21:58:10 +0700

    feat(route): added new edpoints on router

--- route.js
+++ route.js
@@ -3,8 +3,9 @@ import express from 'express';
 
 import { getUser, loginUser, logoutUser, registerUser } from './controllers/AuthController.js';
 
-import { createTraining, getTraining, getTrainings } from './controllers/TrainingController.js';
+import { createTraining, deleteTraining, getTraining, getTrainings, updateTraining } from './controllers/TrainingController.js';
 import { authenticateToken, isManager } from './middleware/authMiddleware.js';
+import { acceptApplication, applyTraining, getApplications, getMyApplications, rejectApplication, withdrawApplication } from './controllers/TrainingApplicationController.js';
 
 const router = express.Router();
 
@@ -39,6 +40,27 @@ router.route('/trainings')
   .post(authenticateToken, isManager, createTraining);
 
 router.route('/training/:id')
-  .get(authenticateToken, getTraining);
+  .get(authenticateToken, getTraining)
+  .put(authenticateToken, isManager, updateTraining)
+  .delete(authenticateToken, isManager, deleteTraining);
+
+// TrainingApplicationController
+router.route('/training-application')
+    .post(authenticateToken, applyTraining)
+    .get(authenticateToken, isManager, getApplications);
+
+router.route('/training-application/:id/withdraw')
+    .patch(authenticateToken, withdrawApplication);
+
+router.route('/my-applications')
+    .get(authenticateToken, getMyApplications);
+
+router.route('/training-application/:id/accept')
+    .patch(authenticateToken, isManager, acceptApplication);
+
+router.route('/training-application/:id/reject')
+    .patch(authenticateToken, isManager, rejectApplication);
+
+
 
 export default router;
